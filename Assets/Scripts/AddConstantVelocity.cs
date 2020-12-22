d02d88ddd8fe73d3d4a4ddd82bc1e22e4ef90ea5
ShaurGaur <shauryavrat@live.com>
2020-12-21 22:15:05 -0800

    added game files

---
+++ Assets/Scripts/AddConstantVelocity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class AddConstantVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // called 100 times per second
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity += v3Force;
+    }
+}
---
+++ Assets/Scripts/EndLevelOnCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevelOnCollision : MonoBehaviour
+{
+    [SerializeField]
+    string strTag;
+    
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == strTag)
+            Application.Quit();
+    }
+}
